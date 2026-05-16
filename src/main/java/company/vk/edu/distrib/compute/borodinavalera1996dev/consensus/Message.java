36d22a7208ae3d641c615f271bb690c8d604c51f
valery <vborodina@emdev.ru>
2026-05-17 02:56:38 +0300

    fix codestyle

--- src/main/java/company/vk/edu/distrib/compute/borodinavalera1996dev/consensus/Message.java
+++ src/main/java/company/vk/edu/distrib/compute/borodinavalera1996dev/consensus/Message.java
@@ -1,5 +1,7 @@
 package company.vk.edu.distrib.compute.borodinavalera1996dev.consensus;
 
 public record Message(int idNode, Type type) {
-    public enum Type { PING, ELECT, ANSWER, VICTORY}
+    public enum Type {
+        PING, ELECT, ANSWER, VICTORY
+    }
 }
